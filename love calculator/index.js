c9f79166edc0ef58791973ea821ecb61eba5a4b9
aarya <aaryashendre1@gmail.com>
2023-09-06 15:13:41 +0530

    love calculator

---
+++ love calculator/index.js
@@ -0,0 +1,35 @@
+const getper = document.getElementById("getper");
+const inpreq = document.getElementById("inpreq");
+const yourper = document.getElementById("yourper");
+const his = document.getElementById("his");
+const her = document.getElementById("her");
+
+
+
+getper.addEventListener("click", () => {
+
+if (his.value == "") {
+    console.log ("please input his name");
+    inpreq.innerText ="please input his name";
+    inpreq.style.color = "red"
+}
+
+else if(her.value == "") {
+    console.log ("please input her name");
+    inpreq.innerText ="please input her name";
+    inpreq.style.color = "red"
+}
+
+ else {
+        let x = Math.floor((Math.random() * 100) + 1);
+        yourper.innerText = `hello ${his.value} & ${her.value} your love percentage is ${x}%. Congratulation !`
+
+}
+})
+
+// console.log(his.value == "" && her.value == "")
+
+// getper.addEventListener("click", () => {
+//     let x = Math.floor((Math.random() * 100) + 1);
+//     yourper.innerText = `hello ${his.value} & ${her.value} your love percentage is ${x}%. Congratulation !`
+// })
