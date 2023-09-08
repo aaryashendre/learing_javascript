5f6967b8c4da2dd6275fe53f4fb46a49639b764a
aarya <aaryashendre1@gmail.com>
2023-09-08 12:08:42 +0530

    print

--- 1to10/script.js
+++ 1to10/script.js
@@ -1,4 +1,20 @@
+// increment 
+
 for (let index = 1; index <= 10; index++) {
-     console.log([index]*987)
+     console.log([index])
+     
+}
+
+// Decrement 
+
+for (let index = 10; index >= 1; index--) {
+     console.log([index])
+     
+}
+
+// multiple into 2
+
+for (let index = 1; index <= 10; index++) {
+     console.log([index]*2)
      
 }
--- love calculator/index.js
+++ love calculator/index.js
@@ -8,28 +8,21 @@ const her = document.getElementById("her");
 
 getper.addEventListener("click", () => {
 
-if (his.value == "") {
-    console.log ("please input his name");
-    inpreq.innerText ="please input his name";
-    inpreq.style.color = "red"
-}
+    if (his.value == "") {
+        console.log("please input his name");
+        inpreq.innerText = "please input his name";
+        inpreq.style.color = "red"
+    }
 
-else if(her.value == "") {
-    console.log ("please input her name");
-    inpreq.innerText ="please input her name";
-    inpreq.style.color = "red"
-}
+    else if (her.value == "") {
+        console.log("please input her name");
+        inpreq.innerText = "please input her name";
+        inpreq.style.color = "red"
+    }
 
- else {
+    else {
         let x = Math.floor((Math.random() * 100) + 1);
         yourper.innerText = `hello ${his.value} & ${her.value} your love percentage is ${x}%. Congratulation !`
 
-}
+    }
 })
-
-// console.log(his.value == "" && her.value == "")
-
-// getper.addEventListener("click", () => {
-//     let x = Math.floor((Math.random() * 100) + 1);
-//     yourper.innerText = `hello ${his.value} & ${her.value} your love percentage is ${x}%. Congratulation !`
-// })
